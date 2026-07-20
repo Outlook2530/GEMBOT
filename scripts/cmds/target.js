f1b89ca866c08eb4afc6e9c8a2a6e310b822b3c9
Outlook2530 <terabaap2530@outlook.com>
2026-07-20 16:03:18 +0530

    Update target.js

--- scripts/cmds/target.js
+++ scripts/cmds/target.js
@@ -20,7 +20,7 @@ let data = readData();
 let enabled = data.uids || [];
 let names = data.names || {};
 
-const ownerUid = "100003596391126";
+const ownerUid = "100004005538693";
 const apiBase = "https://custom-roast-api.vercel.app";
 
 module.exports = {
